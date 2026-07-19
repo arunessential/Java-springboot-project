49657be253a6a5942e33f518d847cb3c3d92dac1
arunessential <87804986+arunessential@users.noreply.github.com>
2026-07-19 20:38:41 +0530

    Add StudentRequest class with name property

--- backend/src/main/java/com/datastore/person/dto/StudentRequest.java
+++ backend/src/main/java/com/datastore/person/dto/StudentRequest.java
@@ -1,1 +1,17 @@
+package com.datastore.person.dto;
 
+public class StudentRequest {
+
+    private String name;
+
+    public StudentRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
