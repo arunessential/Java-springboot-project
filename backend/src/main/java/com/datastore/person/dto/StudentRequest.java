a0af6950ee2f7392b0f3d7a8470327f02d3bf677
arunessential <87804986+arunessential@users.noreply.github.com>
2026-07-19 21:06:34 +0530

    Add validation annotations to StudentRequest fields

--- backend/src/main/java/com/datastore/person/dto/StudentRequest.java
+++ backend/src/main/java/com/datastore/person/dto/StudentRequest.java
@@ -1,8 +1,14 @@
 package com.datastore.person.dto;
 
+import javax.validation.constraints.NotNull;
+
 public class StudentRequest {
 
+    @NotNull
     private String name;
+
+    @NotNull
+    private int age;
 
     public StudentRequest() {
     }
@@ -13,5 +19,13 @@ public class StudentRequest {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
